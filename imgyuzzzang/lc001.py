8708a2c32ab2e3e413a8e2afb4e715e60b5a8370
GyuZzang <tndud0606@gmail.com>
2022-07-25 23:20:29 +0900

    fix(lc001): add default return

--- imgyuzzzang/lc001.py
+++ imgyuzzzang/lc001.py
@@ -6,6 +6,7 @@ def twoSum(nums: List[int], target: int) -> List[int]:
             for j in range(i+1, len(nums)):
                 if (nums[i] + nums[j] == target):
                     return [i,j]
+    return []
 
 
 print(twoSum([2,7,11,15], 9))

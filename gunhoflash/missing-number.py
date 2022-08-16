f2321ea5713a5af5b7ef25dd2cc5414a8459177e
dewy.i <dewy.i@kakaocorp.com>
2022-08-16 23:35:53 +0900

    rename to avoid redefining

--- gunhoflash/missing-number.py
+++ gunhoflash/missing-number.py
@@ -2,5 +2,5 @@ from typing import List
 
 class Solution:
   def missingNumber(self, nums: List[int]) -> int:
-    max = len(nums)
-    return (max * (max + 1) // 2) - sum(nums)
+    n = len(nums)
+    return (n * (n + 1) // 2) - sum(nums)
--- gunhoflash/missing-number.ts
+++ gunhoflash/missing-number.ts
@@ -1,4 +1,4 @@
 function missingNumber(nums: number[]): number {
-  const max = nums.length;
-  return (max * (max + 1) / 2) - nums.reduce((sum, num) => sum + num, 0);
+  const n = nums.length;
+  return (n * (n + 1) / 2) - nums.reduce((sum, num) => sum + num, 0);
 }

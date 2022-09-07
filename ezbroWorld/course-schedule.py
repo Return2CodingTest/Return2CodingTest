1e549e3ad995ae321298821a315235806596e26f
ez-bro <36829127+ez-bro@users.noreply.github.com>
2022-09-07 21:58:42 +0900

    Create course-schedule.py

---
+++ ezbroWorld/course-schedule.py
@@ -0,0 +1,34 @@
+import collections
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:   
+        
+        graph = collections.defaultdict(list)
+        for x, y in prerequisites:
+            graph[x].append(y)
+
+        traced, visited = set(), set()
+        
+        def dfs(i):
+            
+            if i in visited :
+                return True
+            
+            if i in traced : 
+                return False
+            
+            traced.add(i)
+            
+            for j in graph[i]:
+                if not dfs(j):
+                    return False
+                
+            traced.remove(i)
+            visited.add(i)
+            return True
+            
+            
+        for x in list(graph):
+            if not dfs(x):
+                return False
+            return True

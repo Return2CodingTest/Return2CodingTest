bc87db4bad6463ddf668846cf21d08520c93c534
ez-bro <36829127+ez-bro@users.noreply.github.com>
2022-09-21 21:36:52 +0900

    Create number-of-islands.py

---
+++ ezbroWorld/number-of-islands.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ans = 0
+        rowLen = len(grid)
+        colLen = len(grid[0])
+        # visited=[] 으로 하고 if (r,c) in visited 하면 느림
+        visited = [[0 for _ in range(colLen)] for _ in range(rowLen)]
+        
+        def bfs(r,c):
+            
+            if r<0 or r>=rowLen or c<0 or c>=colLen or visited[r][c]==1 :
+                return
+            
+            if grid[r][c] == '0' :
+                return
+            
+            visited[r][c]=1
+
+            bfs(r-1,c)
+            bfs(r+1,c)          
+            bfs(r,c+1)    
+            bfs(r,c-1)
+            
+        for r in range(rowLen):
+            for c in range(colLen):
+                if visited[r][c] == 0 and grid[r][c] == '1' : 
+                    bfs(r,c)
+                    ans+=1
+                    
+        return ans

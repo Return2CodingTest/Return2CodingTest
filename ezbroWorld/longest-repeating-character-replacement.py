5fbb5f3dd8a5a8923709bbde6af7fe2de8a539f3
ez-bro <36829127+ez-bro@users.noreply.github.com>
2022-11-02 21:59:17 +0900

    Create longest-repeating-character-replacement.py

---
+++ ezbroWorld/longest-repeating-character-replacement.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        start=end=0 
+        length = len(s)
+        counter = Counter()
+        
+        for end in range(1,length+1) : 
+            counter[s[end-1]]+=1
+            
+            frequentCnt = counter.most_common(1)[0][1]
+            
+            if end-start-frequentCnt > k : 
+                counter[s[start]]-=1
+                start-=1
+                
+        return end- start
